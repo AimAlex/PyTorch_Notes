78b9b2f9ad9fd80f81c702cf71dac4590fec6dd0
AimAlex <alex-li@sjtu.edu.cn>
2018-08-07 17:18:47 +0800

    I want to push them to the github

---
+++ PyTorch_Codes/305.py
@@ -0,0 +1,37 @@
+import torch
+import torch.utils.data as Data
+
+torch.manual_seed(1)
+
+BATCH_SIZE = 2
+
+x = torch.linspace(1, 10, 10)
+y = torch.linspace(10, 1, 10)
+print(x.numpy())
+print(y.numpy())
+
+torch_dataset = Data.TensorDataset(data_tensor = x, target_tensor = y)
+loader = Data.DataLoader(
+	dataset = torch_dataset,
+	batch_size = BATCH_SIZE,
+	shuffle = True,
+	num_workers = 2
+)
+
+for epoch in range(3):
+	for step, (batch_x, batch_y) in enumerate(loader):
+		print('Epoch: ', epoch, '| Step: ', step, '| batch x: ', batch_x.numpy(), '| batch y: ', batch_y.numpy())
+
+
+BATCH_SIZE = 8
+loader = Data.DataLoader(
+	dataset = torch_dataset,
+	batch_size = BATCH_SIZE,
+	shuffle = True,
+	num_workers = 2
+)
+
+for epoch in range(3):
+	for step, (batch_x, batch_y) in enumerate(loader):
+		print('Epoch: ', epoch, '| Step: ', step, '| batch x: ',
+			batch_x.numpy(), '| batch y: ', batch_y.numpy())
